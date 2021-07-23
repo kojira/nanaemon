b5958ddd3bd766a147427ad36b1f6515ac777349
kojira <kojiran@gmail.com>
2021-07-23 18:51:01 +0900

    過去サウンドが他にないとおかしくなるのでアイコンとユーザーのURL取得方法を変更

--- nana.py
+++ nana.py
@@ -10,8 +10,8 @@ def get_sound_info(url):
     page_info = {}
     page_info["image"] = html.cssselect('meta[property="og:image"]')[0].get('content')
     page_info["user_name"] = html.xpath("//div[@class='post-user-name']")[0].text_content().strip()
-    page_info["icon_url"] = html.xpath("//div[@class='user-thumb']/img")[0].attrib["src"]
-    page_info["user_url"] = "https://nana-music.com"+html.xpath("//div[@class='user-latest-posts__heading']")[0][0].attrib["href"]
+    page_info["icon_url"] = html.xpath("//div[@class='post-user']")[0][0][0].attrib["src"]
+    page_info["user_url"] = "https://nana-music.com"+html.xpath("//div[@class='post-user']")[0][0].attrib["href"]
     sound_url_base = html.xpath("/html/body/script[1]")[0].text_content().strip()
     m = re.search(r"sound_url=\"(.*?\.m4a)\"", sound_url_base)
     page_info["sound_url"] = codecs.decode(m.groups()[0], 'unicode-escape')
